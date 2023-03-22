3a94a4afc69b59fca60f00dd0c4d1e85403ae823
Oxygen033 <102524912+Oxygen033@users.noreply.github.com>
2023-03-22 16:00:52 +0600

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+def Parse():
+    url = "https://www.omgtu.ru"
+    page= requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    file = open("output.txt", "w")
+    while True:
+        block = soup.findAll('h3', class_='news-card__title')
+        description = ''
+        for data in block:
+            description = data.text
+            file.write(description)
+        break
+    file.close()
