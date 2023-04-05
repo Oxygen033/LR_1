7257d4471d0a02b173dc61f671ed172d21e20885
Oxygen033 <102524912+Oxygen033@users.noreply.github.com>
2023-04-05 13:30:47 +0600

    Update parser.py
    Update

--- parser.py
+++ parser.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 def Parse():
-    BaseUrl = "https://www.omgtu.ru/l/?PAGEN_1={}"
+    baseUrl = "https://www.omgtu.ru/l/?PAGEN_1={}"
     file = open("output.txt", "w", encoding='utf-8')
-    for PageNumber in range(1, 158):
-        url = BaseUrl.format(PageNumber)
+    for pageNumber in range(1, 158):
+        url = baseUrl.format(pageNumber)
         page = requests.get(url)
         soup = BeautifulSoup(page.text, "html.parser")
         block = soup.findAll('h3', class_='news-card__title')
@@ -13,3 +13,4 @@ def Parse():
             description = data.text
             file.write(description.strip() + "\n")
     file.close()
+
